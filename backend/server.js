3c8bbba0ec6c89384ecf757ddac07f15c5ec306b
Saket Agrawal <54344211+sam2001x@users.noreply.github.com>
2023-10-05 11:29:43 +0530

    added backend
    adde backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the MERN sample backend!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
