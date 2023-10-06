e9859e34d472d74bba07bd680d17dba969ccf299
Saket Agrawal <54344211+sam2001x@users.noreply.github.com>
2023-10-06 14:11:21 +0530

    Add files via upload

---
+++ scripts/start_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ECR_REO_URL=405052395309.dkr.ecr.ap-south-1.amazonaws.com/sachin-rep
+
+# Authenticating ecr
+$(aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 405052395309.dkr.ecr.ap-south-1.amazonaws.com/sachin-rep)
+
+docker pull 405052395309.dkr.ecr.ap-south-1.amazonaws.com/sachin-rep:latest
+
+docker images
+
+docker run -d -p 5000:5000 405052395309.dkr.ecr.ap-south-1.amazonaws.com/sachin-rep:latest
+
+
---
+++ scripts/stop_previous_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "calling stop container sh"
